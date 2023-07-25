7f67d9376412e92fd2a74d04d566268da8e3dc8e
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2023-07-25 09:57:45 -0300

    entrega final

--- README.md
+++ README.md
@@ -1,49 +1,5 @@
 ### Rodando os containeers
 	```
+	docker network create --subnet=172.20.0.0/24 redis-net
 	docker compose up --build -d
 	```
-
-<!-- 1. Criando a rede:
-	```
-	docker network create redis-net
-	```
-
-2. Criando containers docker: 
-	
-	```
-	docker run --name redis-master -p 6379:6379 --network redis-net -d redis redis-server --appendonly yes
-	docker run --name redis-replica1 -p 6380:6379 --network redis-net -d redis redis-server --appendonly yes --slaveof redis-master 6379
-	docker run --name redis-replica2 -p 6381:6379 --network redis-net -d redis redis-server --appendonly yes --slaveof redis-master 6379
-	```
-
-3. Checando IP dos containers:
-	```
-	docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
-	```
-
-4. Criando os aqrquivos sentinel.conf e ligando ao master (mudar o caminho dos arquivos e o IP do master):
-	```
-	echo "sentinel monitor mymaster 172.18.0.2 6379 2" > ./sentinel_1/sentinel.conf
-	echo "sentinel monitor mymaster 172.18.0.2 6379 2" > ./sentinel_2/sentinel.conf
-	echo "sentinel monitor mymaster 172.18.0.2 6379 2" > ./sentinel_3/sentinel.conf
-	```
-
-5. Criando os containers das sentinels (mudar o caminho dos arquivos):
-	```
-	docker run -d --name redis-sentinel_1 -p 26379:26379 --network redis-net -v ./sentinel_1:/data redis redis-sentinel /data/sentinel.conf
-	docker run -d --name redis-sentinel_2 -p 26380:26379 --network redis-net -v ./sentinel_2:/data redis redis-sentinel /data/sentinel.conf
-	docker run -d --name redis-sentinel_3 -p 26381:26379 --network redis-net -v ./sentinel_3:/data redis redis-sentinel /data/sentinel.conf
-	```
-
-6. Verificando se as sentinelas estão executando corretamente:	
-	```
-	docker container logs --follow redis-sentinel_1
-	docker container logs --follow redis-sentinel_2
-	docker container logs --follow redis-sentinel_3
-	```
-
-6. Executando API node.js (atualizar lista de endpoints):
-	```
-	npm install
-    npm start
-	``` -->
--- nodejs/index.js
+++ nodejs/index.js
@@ -5,55 +5,111 @@ import Redis from 'ioredis';
 const app = express();
 app.use(express.json());
 
-const PORT = 6380;
-
 app.get('/hash/:key', async (req, res) => {
-    const redis = new Redis({ host: "127.0.0.1", port: PORT });
+    const redis = new Redis({ host: '172.20.0.5', port: 26379 });
+    redis.on('error', err => console.log('Error connecting to Redis:', err.message));
 
-    redis.on('error', err => console.log('Error connecting to Redis: ', err));
+    await redis.info().then(async (result) => {
+        let host = result.split('\n').slice(-2, -1)[0].slice(40, 55).split(':');
+        const client = new Redis({ host: host[0], port: host[1] });
 
-    await redis.hgetall(req.params.key).then(result => {
-        res.send(result);
+        client.on('error', err => {
+            console.log('Error connecting to Redis:', err.message)
+        });
+
+        await client.hgetall(req.params.key).then(result => {
+            res.send(result)
+        }).catch(err => {
+            res.status(500).send(err.message);
+        });
     }).catch(err => {
-        res.status(500).send(err.message);
+        console.log(err.message);
+    });
+});
+
+app.get('/keys', async (req, res) => {
+    const redis = new Redis({ host: '172.20.0.5', port: 26379 });
+    redis.on('error', err => console.log('Error connecting to Redis:', err.message));
+
+    var teste = []
+    await redis.info().then(async (result) => {
+        teste = result.split('\n').slice(-2, -1)[0].slice(40, 55).split(':');
+        const client = createClient({ url: `redis://${teste[0] == '172.20.0.2' ? (teste[0] == '172.20.0.3' ? '172.20.0.4' : '172.20.0.3') : '172.20.0.2'}`, port: teste[1] });
+
+        client.on('error', err => console.log('Error connecting to Redis: ', err));
+        client.connect();
+
+        await client.keys('*').then(result => {
+            res.send(result);
+        }).catch(err => {
+            res.status(500).send(err.message);
+        }).finally(() => client.disconnect());
+    }).catch(err => {
+        console.log(err.message);
     });
 });
 
 app.post('/hash', async (req, res) => {
-    const redis = new Redis({ host: "127.0.0.1", port: PORT });
+    const redis = new Redis({ host: '172.20.0.5', port: 26379 });
+    redis.on('error', err => console.log('Error connecting to Redis:', err.message));
 
-    redis.on('error', err => console.log('Error connecting to Redis: ', err));
+    var teste = []
+    await redis.info().then(async (result) => {
+        teste = result.split('\n').slice(-2, -1)[0].slice(40, 55).split(':');
+        const client = new Redis({ host: teste[0], port: teste[1] });
 
-    await redis.hset(req.body.key, req.body.fields).then(result => {
-        res.send('criado');
+        client.on('error', err => console.log('Error connecting to Redis: ', err));
+
+        await client.hset(req.body.key, req.body.fields).then(result => {
+            res.send('criado');
+        }).catch(err => {
+            res.status(500).send(err.message);
+        });
     }).catch(err => {
-        res.status(500).send(err.message);
+        console.log(err.message);
     });
 });
 
 app.put('/hash/:key', async (req, res) => {
-    const redis = new Redis({ host: "127.0.0.1", port: PORT });
+    const redis = new Redis({ host: '172.20.0.5', port: 26379 });
+    redis.on('error', err => console.log('Error connecting to Redis:', err.message));
 
-    redis.on('error', err => console.log('Error connecting to Redis: ', err));
+    var teste = []
+    await redis.info().then(async (result) => {
+        teste = result.split('\n').slice(-2, -1)[0].slice(40, 55).split(':');
+        const client = new Redis({ host: teste[0], port: teste[1] });
 
+        client.on('error', err => console.log('Error connecting to Redis: ', err));
 
-    await redis.hset(req.params.key, req.body).then(result => {
-        res.send('atualizado');
+        await client.hset(req.params.key, req.body).then(result => {
+            res.send(teste);
+        }).catch(err => {
+            res.status(500).send(err.message);
+        });
     }).catch(err => {
-        res.status(500).send(err.message);
+        console.log(err.message);
     });
 });
 
 app.delete('/hash/:key', async (req, res) => {
-    const redis = new Redis({ host: "127.0.0.1", port: PORT });
+    const redis = new Redis({ host: '172.20.0.5', port: 26379 });
+    redis.on('error', err => console.log('Error connecting to Redis:', err.message));
 
-    redis.on('error', err => console.log('Error connecting to Redis: ', err));
+    var teste = []
+    await redis.info().then(async (result) => {
+        teste = result.split('\n').slice(-2, -1)[0].slice(40, 55).split(':');
+        const client = new Redis({ host: teste[0], port: teste[1] });
 
-    await redis.del(req.params.key).then(result => {
-        res.send('deletado');
+        client.on('error', err => console.log('Error connecting to Redis: ', err));
+
+        await client.del(req.params.key).then(result => {
+            res.send('deletado');
+        }).catch(err => {
+            res.status(500).send(err.message);
+        });
     }).catch(err => {
-        res.status(500).send(err.message);
+        console.log(err.message);
     });
 });
 
-app.listen(4001);
+app.listen(3000);
